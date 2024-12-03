2f272c011cd023ae3672fd8389f69da736a158b7
gltjk <irontjk@gmail.com>
2024-12-04 00:13:17 +0800

    feat: day 3

---
+++ src/solutions/03.ts
@@ -0,0 +1,15 @@
+/**
+ * Day 3: Mull It Over
+ * @see https://adventofcode.com/2024/day/3
+ */
+
+export default function (input: string, level: 1 | 2) {
+  const puzzle = input.split("\n").join("");
+  if (level === 1) return mul(puzzle);
+  return mul(puzzle.replace(/don't\(\).*?do\(\)/g, ""));
+}
+
+function mul(input: string) {
+  return input.matchAll(/mul\((\d+),(\d+)\)/g)
+    .reduce((acc, [_, a, b]) => acc + Number(a) * Number(b), 0);
+}

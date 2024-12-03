f7cdf59777ff0825e69769da108daa1f49a443cb
gltjk <irontjk@gmail.com>
2024-12-04 00:12:43 +0800

    feat: day 1

---
+++ src/solutions/01.ts
@@ -0,0 +1,30 @@
+/**
+ * Day 1: Historian Hysteria
+ * @see https://adventofcode.com/2024/day/1
+ */
+
+export default function (input: string, level: 1 | 2) {
+  const left: number[] = [];
+  const right: number[] = [];
+  for (const row of input.split("\n")) {
+    const [l, r] = row.split(/\s+/);
+    left.push(Number(l));
+    right.push(Number(r));
+  }
+  if (level === 1) return level1(left, right);
+  return level2(left, right);
+}
+
+function level1(left: number[], right: number[]) {
+  left = left.toSorted((x, y) => x - y);
+  right = right.toSorted((x, y) => x - y);
+  return left.reduce((acc, x, i) => acc + Math.abs(x - right[i]), 0);
+}
+
+function level2(left: number[], right: number[]) {
+  const rightCount: Record<number, number> = {};
+  for (const r of right) {
+    rightCount[r] = (rightCount[r] ?? 0) + 1;
+  }
+  return left.reduce((acc, x) => acc + x * (rightCount[x] ?? 0), 0);
+}

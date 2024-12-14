2d192d04753a78093adc449ca311ab000b654eaf
gltjk <irontjk@gmail.com>
2024-12-14 12:27:03 +0800

    feat: day 13 part 2 (should have solved linear equations earlier)

--- src/solutions/2024/202413.ts
+++ src/solutions/2024/202413.ts
@@ -19,41 +19,12 @@ export default function solve(input: string, level: 1 | 2) {
     };
   });
 
-  let sum = 0;
-  for (const { a, b, prize } of machines) {
-    const { gcd: gcdY } = extendedGCD(a.y, b.y);
-    if (prize.y % gcdY) continue;
-
-    let min = Infinity;
-
-    for (const pos of getPos(a.x, b.x, prize.x)) {
-      if (a.y * pos.x + b.y * pos.y !== prize.y) continue;
-      min = Math.min(min, pos.x * 3 + pos.y);
-      break;
-    }
-    if (min !== Infinity) sum += min;
-  }
-  return sum;
-}
-
-function extendedGCD(
-  a: number,
-  b: number,
-): { gcd: number; x: number; y: number } {
-  if (!a) return { gcd: b, x: 0, y: 1 };
-  const { gcd, x, y } = extendedGCD(b % a, a);
-  return { gcd, x: y - Math.floor(b / a) * x, y: x };
-}
-
-function* getPos(a: number, b: number, target: number) {
-  const { gcd, x, y } = extendedGCD(a, b);
-  if (target % gcd) return;
-  const k = target / gcd;
-  const [x0, y0] = [x * k, y * k];
-  const [dx, dy] = [b / gcd, -a / gcd];
-  for (let n = Math.ceil(-x0 / dx); n <= Math.floor(-y0 / dy); n++) {
-    const [xn, yn] = [x0 + n * dx, y0 + n * dy];
-    if (xn <= 0 || yn <= 0) continue;
-    yield { x: xn, y: yn };
-  }
+  return machines.reduce((sum, { a, b, prize }) => {
+    const det = a.x * b.y - a.y * b.x;
+    if (!det) return sum;
+    const x = (prize.x * b.y - prize.y * b.x) / det;
+    const y = (a.x * prize.y - a.y * prize.x) / det;
+    if (x % 1 || y % 1 || x < 0 || y < 0) return sum;
+    return sum + 3 * x + y;
+  }, 0);
 }

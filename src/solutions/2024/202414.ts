3ccb0207e7d9a8eba93a2914167fde3bee931c31
gltjk <irontjk@gmail.com>
2024-12-14 13:43:46 +0800

    feat: day 14 part 2

--- src/solutions/2024/202414.ts
+++ src/solutions/2024/202414.ts
@@ -7,6 +7,8 @@
 
 import { Vector } from "helpers";
 
+type Robot = { p: Vector; v: Vector };
+
 export default function solve(
   input: string,
   level: 1 | 2,
@@ -17,26 +19,38 @@ export default function solve(
     return { p: new Vector(px, py), v: new Vector(vx, vy) };
   });
 
-  const quadrants: Record<string, number> = { NE: 0, SE: 0, SW: 0, NW: 0 };
-
-  for (let t = 0; t < 100; t++) {
-    for (const robot of robots) {
-      robot.p = robot.p.add(robot.v);
-      while (robot.p.x < 0) robot.p.x += bound.x;
-      while (robot.p.x >= bound.x) robot.p.x -= bound.x;
-      while (robot.p.y < 0) robot.p.y += bound.y;
-      while (robot.p.y >= bound.y) robot.p.y -= bound.y;
+  if (level === 1) {
+    for (let t = 0; t < 100; t++) moveRobots(robots, bound);
+    const halfBound = { x: (bound.x - 1) / 2, y: (bound.y - 1) / 2 };
+    const quadrants: Record<string, number> = { NE: 0, SE: 0, SW: 0, NW: 0 };
+    for (const { p } of robots) {
+      const q1 = p.y < halfBound.y ? "N" : p.y > halfBound.y ? "S" : "";
+      const q2 = p.x < halfBound.x ? "W" : p.x > halfBound.x ? "E" : "";
+      const quadrant = `${q1}${q2}`;
+      if (quadrant.length > 1) quadrants[quadrant]++;
     }
+    return quadrants.NE * quadrants.SE * quadrants.SW * quadrants.NW;
   }
 
-  const halfBound = { x: (bound.x - 1) / 2, y: (bound.y - 1) / 2 };
-
-  for (const { p } of robots) {
-    const q1 = p.y < halfBound.y ? "N" : p.y > halfBound.y ? "S" : "";
-    const q2 = p.x < halfBound.x ? "W" : p.x > halfBound.x ? "E" : "";
-    const quadrant = `${q1}${q2}`;
-    if (quadrant.length > 1) quadrants[quadrant]++;
+  let t = 0;
+  while (!isXmasTree(robots)) {
+    moveRobots(robots, bound);
+    t++;
   }
+  return t;
+}
 
-  return quadrants.NE * quadrants.SE * quadrants.SW * quadrants.NW;
+function moveRobots(robots: Robot[], bound: { x: number; y: number }) {
+  for (const robot of robots) {
+    robot.p = robot.p.add(robot.v);
+    while (robot.p.x < 0) robot.p.x += bound.x;
+    while (robot.p.x >= bound.x) robot.p.x -= bound.x;
+    while (robot.p.y < 0) robot.p.y += bound.y;
+    while (robot.p.y >= bound.y) robot.p.y -= bound.y;
+  }
+}
+
+function isXmasTree(robots: Robot[]) {
+  const set = new Set(robots.map((x) => x.p.toString()));
+  return set.size === robots.length;
 }
